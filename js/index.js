b09e44bdf0be24a2677748a7704280daffd81cb0
Михаил <misha-dobatkin@mail.ru>
2019-10-16 23:34:46 +0300

    сделана панель навигации

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let btn = document.querySelector('.btn');
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function(event){
+    event.preventDefault();
     btn.classList.toggle('btn_active');
 })
