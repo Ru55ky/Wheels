660ef5c55a671221e472df3dc1ed09b41e150f2d
Михаил <misha-dobatkin@mail.ru>
2019-10-22 18:13:30 +0300

    исправлена ошибка контейнера с велосипедами, добавлен первый hover эффект в Social

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ btn.addEventListener("click", function(event){
     event.preventDefault();
     btn.classList.toggle('btn_active');
 })
+
+let imgText = document.querySelector('.text-none');
+let imgBike1 = document.querySelector('.gallery-bike1');
+imgBike1.addEventListener("mouseover", function(evt){
+    evt.preventDefault();
+    imgText.classList.remove('text-none');
+    imgText.classList.add('gallery-bike1-text');
+})
+imgBike1.addEventListener("mouseout", function (evt) { 
+    evt.preventDefault();
+    imgText.classList.remove('gallery-bike1-text')
+    imgText.classList.add('text-none')
+ })
